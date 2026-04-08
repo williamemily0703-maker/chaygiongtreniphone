99a89a02929b1672b65331ef20c842b7e84a37a3
williamemily0703-maker <williamemily0703@gmail.com>
2026-04-08 17:43:45 +0700

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,4 +1,3 @@
-
 document.getElementById('toggleEditBtn').addEventListener('click', () => {
   chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
     chrome.tabs.sendMessage(tabs[0].id, {action: "toggleEdit"});
